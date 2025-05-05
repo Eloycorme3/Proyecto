b78387309135787040c566481216939d057fb1e2
Formacion3 <formacion3@aracnosoft.es>
2025-05-05 13:33:18 +0200

    Modificación length imagen bd

--- bd_nimesuki.sql
+++ bd_nimesuki.sql
@@ -8,23 +8,23 @@ CREATE TABLE Anime (
   categorias VARCHAR(150),
   anho_salida INT,
   descripcion VARCHAR(1000) NOT NULL,
-  imagen VARCHAR(30),
+  imagen VARCHAR(100),
   cap_totales INT NOT NULL
 );
 
 CREATE TABLE Usuario (
   id_usuario INT PRIMARY KEY auto_increment, 
-  nombre VARCHAR(30) NOT NULL,
+  nombre VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   contrasenha VARCHAR(60) NOT NULL,
   tipo ENUM('ADMIN', 'USER') NOT NULL DEFAULT 'USER'
 );
 
 CREATE TABLE Favoritos (
 	id_anime_FK INT,
-    id_usuario_FK INT,
-    valoracion INT,       
-    cap_actual INT,        
-    PRIMARY KEY (id_usuario_FK, id_anime_FK),
+  id_usuario_FK INT,
+  valoracion INT,       
+  cap_actual INT,        
+  PRIMARY KEY (id_usuario_FK, id_anime_FK),
 	FOREIGN KEY (id_anime_FK) REFERENCES anime(id_anime),
-    FOREIGN KEY (id_usuario_FK) REFERENCES usuario(id_usuario)
+  FOREIGN KEY (id_usuario_FK) REFERENCES usuario(id_usuario)
 );
