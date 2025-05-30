667f6f8f9478bd54be8afc040177b0155cad90f4
Eloycorme3 <163692538+Eloycorme3@users.noreply.github.com>
2025-05-30 16:47:19 +0200

    Update bd_nimesuki.sql

--- bd_nimesuki.sql
+++ bd_nimesuki.sql
@@ -20,12 +20,12 @@ CREATE TABLE Usuario (
 );
 
 CREATE TABLE Favoritos (
-	id_anime_FK INT,
+  id_anime_FK INT,
   id_usuario_FK INT,
-  valoracion INT,       
+  valoracion FLOAT,       
   cap_actual INT,        
   PRIMARY KEY (id_usuario_FK, id_anime_FK),
-	FOREIGN KEY (id_anime_FK) REFERENCES anime(id_anime),
+  FOREIGN KEY (id_anime_FK) REFERENCES anime(id_anime),
   FOREIGN KEY (id_usuario_FK) REFERENCES usuario(id_usuario)
 );
 
